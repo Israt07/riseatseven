fc1b1a0722b3cac29359f9d47d3d6fc2f38e3975
Israt07 <82527970+Israt07@users.noreply.github.com>
2026-05-12 03:54:18 +0600

    Revert "configure static export for deployment to /riseatseven"
    This reverts commit 61f9a02358ae8edf73fea763d9a44686c7256a2a.

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
   basePath: '/riseatseven',
-  assetPrefix: '/riseatseven/',
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
